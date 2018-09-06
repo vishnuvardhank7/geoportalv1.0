429756bffca2806768932e81150afe15a2f4ea14
VISHNUVARDHAN KESAVAN <41356490+vishnuvardhank7@users.noreply.github.com>
2018-09-06 18:08:54 +0530

    Form Update

--- src/geoportal/arcgis_js_api/3.17/esri/dijit/metadata/types/iso/gmd/metadataEntity/Classifi_HierarchyAttribute.js
+++ src/geoportal/arcgis_js_api/3.17/esri/dijit/metadata/types/iso/gmd/metadataEntity/Classifi_HierarchyAttribute.js
@@ -3,7 +3,7 @@
 //>>built
 require({
 	cache: {
-		"url:esri/dijit/metadata/types/iso/gmd/metadataEntity/templates/Classifi_HierarchyAttribute.html": '\x3cdiv data-dojo-attach-point\x3d"containerNode"\x3e\r\n\r\n                    \x3cdiv data-dojo-type\x3d"esri/dijit/metadata/form/Element"\r\n     data-dojo-props\x3d"target:\'gmd:classify_HierarchysAttribute\',minOccurs:1,\r\nlabel:\'${i18nIso.classificationSection.hierarchylevel}\'"\x3e\r\n     \x3cdiv data-dojo-type\x3d"esri/dijit/metadata/form/iso/GcoElement"\r\n     data-dojo-props\x3d"target:\'gmd:classify_hierarchylevelname\'"\x3e \x3c/div \x3e\r\n \x3c/div\x3e \r\n\r\n                                        \x3c/div\x3e'
+		"url:esri/dijit/metadata/types/iso/gmd/metadataEntity/templates/Classifi_HierarchyAttribute.html": '\x3cdiv data-dojo-attach-point\x3d"containerNode"\x3e\r\n\r\n                    \x3cdiv data-dojo-type\x3d"esri/dijit/metadata/form/OpenElement"\r\n     data-dojo-props\x3d"target:\'gmd:classify_HierarchysAttribute\',minOccurs:0,\r\nlabel:\'${i18nIso.classificationSection.hierarchylevel}\'"\x3e\r\n     \x3cdiv data-dojo-type\x3d"esri/dijit/metadata/form/iso/GcoElement"\r\n     data-dojo-props\x3d"target:\'gmd:classify_hierarchylevelname\'"\x3e \x3c/div \x3e\r\n \x3c/div\x3e \r\n\r\n                                        \x3c/div\x3e'
 	}
 });
 define("esri/dijit/metadata/types/iso/gmd/metadataEntity/Classifi_HierarchyAttribute", "dojo/_base/declare dojo/_base/lang dojo/has ../../../../base/Descriptor ../../../../form/Element ../../../../form/iso/AbstractObject ../../../../form/iso/GcoElement dojo/text!./templates/Classifi_HierarchyAttribute.html ../../../../../../kernel".split(" "), function(a, b, c, d, g, h, k, e, f) {
